4aeed0621c533696001fdcada82ac443edb7de22
leo-alexander <leo.mclay@gmail.com>
2018-06-06 17:06:45 +0200

    Initial commit

---
+++ neuraum/src/App/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './app.css';
+
+import Header from "../Header";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Hello World
+        <Header />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ neuraum/src/Header/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+// import PropTypes from "prop-types";
+
+import styles from "./header.css";
+
+const Header = () => (
+  <div className={styles.container}>This is the header</div>
+)
+
+export default Header
